4e2edfa807416752deb8b8e18503367a346c8fc3
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-10-10 22:17:29 +0100

    completed the logic to add new items to the list

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 let myNodeList = document.getElementsByTagName('LI');
 
 let i;
@@ -6,4 +8,7 @@ for (i = 0; i < myNodeList.length; i++) {
 
     let span = document.createElement('SPAN');
     let txt = document.createTextNode('\u00D7');
+    span.className = 'close'
+    span.appendChild('txt');
+    myNodeList[i].appendChild(span)
 }
